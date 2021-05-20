8670d782eb829da3bddb871e03972ad1426bbfbf
KyeongSang Yu <35093128+ykss@users.noreply.github.com>
2021-05-21 00:41:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('테스트 실행')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ridibooks.com/category/new-releases/2200'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+bookservices = soup.select('.title_text')
+for no, book in enumerate(bookservices, 1):
+    print(no, book.text.strip())
