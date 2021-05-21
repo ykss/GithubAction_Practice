aa155bfd2e31c8406080b0bfc78138eede30229b
KyeongSang Yu <35093128+ykss@users.noreply.github.com>
2021-05-21 22:41:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+import json
 
-url = 'https://ridibooks.com/category/new-releases/2200'
-response = requests.get(url)
+response = requests.get("http://paullab.synology.me/stock.html")
+
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.title_text')
-for no, book in enumerate(bookservices, 1):
-    print(no, book.text.strip())
+oneStep = soup.select('.main')[2]
+twoStep = oneStep.select('tbody > tr')[1:]
+
+날짜 = []
+종가 = []
+전일비 = []
+거래량 = []
+
+for i in twoStep:
+    날짜.append(i.select('td')[0].text)
+    종가.append(int(i.select('td')[1].text.replace(',', '')))
+    전일비.append(int(i.select('td')[2].text.replace(',', '')))
+    거래량.append(int(i.select('td')[6].text.replace(',', '')))
+
+l = []
+
+for i in range(len(날짜)):
+    l.append({
+        '날짜':날짜[i],
+        '종가':종가[i],
+        '전일비':전일비[i],
+        '거래량':거래량[i],
+        })
+
+#파일을 한 번 쓴다.
+with open('data.js', "w", encoding="UTF-8-sig") as f_write:
+    json.dump(l, f_write, ensure_ascii=False, indent=4)
+
+#파일을 다시 읽는다.
+data = ""
+with open('data.js', "r", encoding="UTF-8-sig") as f:
+    line = f.readline()
+    while line:
+        data += line
+        line = f.readline()
+
+#파일에 변수명을 추가하여 다시 쓴다.
+final_data = f"var data = {data};"
+with open('data.js', "w", encoding="UTF-8-sig") as f_write:
+    f_write.write(final_data)
